7eb25f73d99d4708ebb8ebcfcdc0617d5153050c
Alexander Marcelo Dos Reis De Almeida <helena.perdigueiro@gmail.com>
2021-11-23 11:18:52 -0300

    primeiro commit

---
+++ src/test/java/com/helena/jenkins2/Jenkins2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helena.jenkins2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jenkins2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
